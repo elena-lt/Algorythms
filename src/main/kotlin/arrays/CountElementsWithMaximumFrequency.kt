ab137390a58898e4b695c110c6f424e4a7f450dd
Lena Lutsiuk <elenalutsiuk5@gmail.com>
2025-02-27 18:57:42 +0100

    CountElementsWithMaximumFrequency

--- src/main/kotlin/arrays/CountElementsWithMaximumFrequency.kt
+++ src/main/kotlin/arrays/CountElementsWithMaximumFrequency.kt
@@ -1,11 +1,17 @@
 package arrays
 
-import kotlin.math.max
-
 /**
  * You are given an array nums consisting of positive integers.
  * Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
  * The frequency of an element is the number of occurrences of that element in the array.
+ *
+ * Example: Input: nums = [1,2,2,3,1,4]. Output: 4
+ *
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 1 <= nums[i] <= 100
+ *
+ * Source: https://leetcode.com/problems/count-elements-with-maximum-frequency/description/?envType=problem-list-v2&envId=array
  */
 fun maxFrequencyElements(nums: IntArray): Int {
     val freg = mutableMapOf<Int, Int>()
